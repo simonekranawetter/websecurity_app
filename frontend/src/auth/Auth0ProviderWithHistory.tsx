8db9a6a27766d3d00dab36878ea66c259fae45a1
Simone Kranawetter <simonekranawetter@gmail.com>
2022-11-22 20:08:15 +0100

    :recycle: add callback to auth

--- frontend/src/auth/Auth0ProviderWithHistory.tsx
+++ frontend/src/auth/Auth0ProviderWithHistory.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 const Auth0ProviderWithHistory = ({ children }: any) => {
+  const navigate = useNavigate();
+
   const domain = process.env.REACT_APP_AUTH0_DOMAIN as string;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID as string;
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE as string;
+
+  const onRedirectCallback = (appState: any) => {
+    navigate(appState?.returnTo || window.location.pathname)
+  }
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
       audience={audience}>
       {children}
     </Auth0Provider>
