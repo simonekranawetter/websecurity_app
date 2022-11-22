b8f3fe9266c88a8b2fcb6e92f7ffe876cfc57064
Simone Kranawetter <simonekranawetter@gmail.com>
2022-11-22 19:50:32 +0100

    :recycle: encoding backend

--- backendAPI/backendAPI/Mappings/MessageMappingExtensions.cs
+++ backendAPI/backendAPI/Mappings/MessageMappingExtensions.cs
@@ -1,13 +1,15 @@
-﻿using backendAPI.DTO;
+﻿using Azure;
+using backendAPI.DTO;
 using backendAPI.Entities;
 using System.Web;
 
 namespace backendAPI.Mappings
 {
     public static class MessageMappingExtensions
-    {   
-        public static MessageEntity MapToEntity(this AddMessageDto dto)
+    {
+            public static MessageEntity MapToEntity(this AddMessageDto dto)
         {
+            var _tagsAllowed = new string[] { "<b>", "</b>", "<i>", "</i>" };
             var entity = new MessageEntity
             {
                 Id = new Guid(),
@@ -16,11 +18,17 @@ namespace backendAPI.Mappings
                 ImgUrl = dto.ImgUrl,
             };
 
+            foreach (var tag in _tagsAllowed)
+            {
+                var encodedTag = HttpUtility.HtmlEncode(tag);
+                entity.Body = entity.Body.Replace(encodedTag, tag);
+            }
             return entity;
         }
 
         public static MessageDto MapToDto(this MessageEntity entity)
         {
+            var _tagsAllowed = new string[] { "<b>", "</b>", "<i>", "</i>" };
             var dto = new MessageDto
             {
                 Id = entity.Id,
@@ -28,7 +36,11 @@ namespace backendAPI.Mappings
                 Body = HttpUtility.HtmlEncode(entity.Body),
                 ImgUrl = entity.ImgUrl,
             };
-            
+            foreach (var tag in _tagsAllowed)
+            {
+                var encodedTag = HttpUtility.HtmlEncode(tag);
+                dto.Body = dto.Body.Replace(encodedTag, tag);
+            }
             return dto;
         }
 
